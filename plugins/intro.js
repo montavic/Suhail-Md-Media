d62f12c9dd73beb4d61aec731f29e80ccfc30442
montavic <46831555+montavic@users.noreply.github.com>
2024-04-01 16:14:01 +0300

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -46,20 +46,19 @@ const { smd, Config,smdBuffer,  prefix } = require('../lib')
 
 
 var surl = 'https://github.com/SuhailTechInfo/Secktor-bot' // Source URL
-const number = '923184474176'
-var name = ' Suhail Tech'
+const number = '255757917366'
+var name = 'frankmontana'
 var body = '𝑇𝛩𝑈𝐶𝛨 𝛨𝛯𝑅𝛯'
 var image = 'https://telegra.ph/file/1e60489705c851f74b55e.jpg'
 let text = `╭═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄
 │       「 𝗠𝗬 𝗜𝗡𝗧𝗥𝗢 」
-│ Name      : sυнαɪʟ ᴛᴇᴄʜ ɪиғօ
-│ Place       : ʟαнᴏʀᴇ ᴘᴋ
-│ Gender    :  ᴍᴀʟᴇ
+│ Name      : frankmontana 
+│ Place       : Tanzania 
+│ Gender    :  male
 │ Age          : 22_
-│ Phone     : wa.me/923184474176
-│ Youtube   : Youtube.com/c/SuhailTechInfo
-│ Status     : ɢᴀᴍᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ
-╰═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄`
+│ Phone     : wa.me/255757917366
+│ Youtube   : 
+│ Status     : Single`
 
 
 
